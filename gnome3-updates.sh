ab4bb428951c31fcae6e672a149a3bce38e6ba86
Dave Behnke <dbehnke74@gmail.com>
2012-05-17 09:49:16 -0400

    Adding Gnome3 updates

---
+++ gnome3-updates.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository -y ppa:gnome3-team/gnome3
+sudo add-apt-repository -y ppa:webupd8team/gnome3
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gnome-shell-extensions gnome-shell-extensions-common
