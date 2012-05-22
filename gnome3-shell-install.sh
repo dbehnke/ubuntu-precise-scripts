c2a272fd22426e1b88d66c51b585d61c6d7cc0af
Dave Behnke <dbehnke@fusion.(none)>
2012-05-22 11:30:04 -0400

    gnome3-shell-install updates

--- gnome3-shell-install.sh
+++ gnome3-shell-install.sh
@@ -2,4 +2,4 @@ sudo add-apt-repository -y ppa:gnome3-team/gnome3
 sudo add-apt-repository -y ppa:webupd8team/gnome3
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install gnome-shell-extensions gnome-shell-extensions-common
+sudo apt-get install gnome-shell gnome-shell-extensions gnome-shell-extensions-common
