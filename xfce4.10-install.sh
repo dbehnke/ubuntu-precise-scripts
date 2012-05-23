3f7df9196d8c8ae16a625750f40bfbf9b2835a15
Dave Behnke <dbehnke@fusion.(none)>
2012-05-23 14:22:15 -0400

    adding greybird themes to xfce 4.10

--- xfce4.10-install.sh
+++ xfce4.10-install.sh
@@ -1,5 +1,7 @@
 sudo add-apt-repository -y ppa:xubuntu-dev/xfce-4.10
+sudo add-apt-repository -y ppa:shimmerproject/ppa
 sudo apt-get update
+sudo apt-get install greybird-themes
 sudo apt-get dist-upgrade
 
 #If after running "dist-upgrade", you get 3 held back packages which can't be upgraded, open Synaptic Package Manager (it's installed by default in Xubuntu), click the "Mark all upgrades" button, then "Apply". Synaptic will ask to remove one package and install a new one - select yes and you're done.
